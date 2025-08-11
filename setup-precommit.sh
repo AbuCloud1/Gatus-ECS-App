cc916ab0d6ab7322c34d0f1ac22f8558404d5129
abubker.omer <Abubker.Omer@magairports.com>
2025-08-11 23:46:57 +0100

    Simplify CI and pre-commit for less friction

--- setup-precommit.sh
+++ setup-precommit.sh
@@ -1,17 +1,8 @@
 #!/bin/bash
 
-echo "Setting up pre-commit hooks..."
+echo "Setting up pre-commit hooks"
 
-# Install pre-commit if not already installed
-if ! command -v pre-commit &> /dev/null; then
-    echo "Installing pre-commit..."
-    pip install pre-commit
-else
-    echo "pre-commit already installed"
-fi
-
-# Install the git hooks
+pip install pre-commit
 pre-commit install
 
-echo "Pre-commit setup complete!"
-echo "Your commits will now automatically run quality checks"
+echo "Pre-commit setup complete"
